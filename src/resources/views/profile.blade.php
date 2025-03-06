1f440c2c539e2b82b7bd5ec2e73f6a5ecb8ea7b6
obana ryota <obanaryota0907@gmail.com>
2025-03-07 07:23:05 +0900

    create ItemController

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
+@section('header')
+@include('layouts.header')
+@endsection
+
 @section('content')
 <h2>プロフィール編集ページ</h2>
-<form action="/logout" method="post">
-@csrf
-<button type="submit">ログアウト</button>
-</form>
 @endsection
