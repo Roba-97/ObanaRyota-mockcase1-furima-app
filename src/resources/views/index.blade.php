9b8a1a6e91c95f86a59da6cffa7b09a7a2ba105f
obana ryota <obanaryota0907@gmail.com>
2025-03-07 19:30:36 +0900

    create index page design

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css')}}">
+@endsection
+
 @section('header')
 @include('layouts.header')
 @endsection
 
 @section('content')
-<h2>アイテム一覧ページ</h2>
+<div class="tab">
+	<form class="tab__form" action="/" method="get">
+		<button class="tab__btn" type="submit">おすすめ</button>
+	</form>
+	<form class="tab__form" action="/?tab=mylist" method="get">
+		<button class="tab__btn" type="submit">マイリスト</button>
+	</form>
+</div>
+<div class="item-list">
+	@for($i = 0; $i < 8; $i++) 
+	<div class="item-list__item">
+		<img src="https://placehold.jp/d9d9d9/000/190x190.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F" alt="商品画像" class="item-list__img">
+		<p class="item-list__name">商品名</p>
+	</div>
+	@endfor
+</div>
 @endsection
