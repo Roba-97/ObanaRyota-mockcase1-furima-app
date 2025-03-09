09c60f22fb85fa047efd97219b8befd784681fb9
obana ryota <obanaryota0907@gmail.com>
2025-03-09 14:49:37 +0900

    fix item detail design

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -15,10 +15,20 @@
         <img src="{{ $item->image_path }}" alt="商品画像">
     </div>
     <div class="item-detail__detail">
-        <div class="item-detail__ttl">
+        <div class="item-detail__top">
             <h2 class="item-detail__name">{{ $item->name }}</h2>
             <p class="item-detail__brand">ブランド名</p>
             <p class="item-detail__price">￥<span>{{ number_format($item->price) }}</span>(税込)</p>
+        </div>
+        <div class="item-detail__icon">
+            <div class="item-detail__icon-like">
+                <img class="icon__img" src="{{ asset('images/star.png') }}" alt="">
+                <div class="icon__count">3</div>
+            </div>
+            <div class="item-detail__icon-comment">
+                <img class="icon__img" src="{{ asset('images/comment.png') }}" alt="">
+                <div class="icon__count">1</div>
+            </div>
         </div>
         <form class="item-detail__purchase" action="/purcahse/{{ $item->id }}" method="get">
             <button class="item-detail__purchase-btn" type="submit">購入手続きへ</button>
@@ -32,7 +42,7 @@
             <div class="item-datail__info-category">
                 <div class="info-category__heading"><p>カテゴリー</p></div>
                 <div class="info-category__list">
-                    <span>洋服</span><span>メンズ</span>
+                    <span>洋服</span><span>メンズ</span><span>ファッション</span>
                 </div>
             </div>
             <div class="item-detail__info-condition">
@@ -41,7 +51,7 @@
             </div>
         </div>
         <div class="item-detail__comment">
-            <h2 class="item-detail__comment-heading">コメント</h2>
+            <h2 class="item-detail__comment-heading">コメント(1)</h2>
             <div class="item-detail__comment-content">
                 <img src="https://placehold.jp/150x150.png" alt="ユーザアイコン" class="comment__user-img">
                 <span class="comment__user-name">admin</span>
@@ -49,8 +59,8 @@
             </div>
             <form class="item-detail__comment-form" action="" method="post">
                 @csrf
-                <label class="comment-form_label" for="">商品へのコメント</label>
-                <textarea class="comment-form__input" name="content" id="" rows="10"></textarea>
+                <label class="comment-form_label" for="input-comment">商品へのコメント</label>
+                <textarea class="comment-form__input" name="content" id="input-comment" rows="10"></textarea>
                 <button class="comment-form__btn" type="submit">コメントを送信する</button>
             </form>
         </div>
