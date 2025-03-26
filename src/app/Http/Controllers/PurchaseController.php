9b5e60800acbbcb62be388cd1d7fc8ae9dfee469
obana ryota <obanaryota0907@gmail.com>
2025-03-26 15:52:15 +0900

    fix payment option binding

--- src/app/Http/Controllers/PurchaseController.php
+++ src/app/Http/Controllers/PurchaseController.php
@@ -14,6 +14,7 @@ class PurchaseController extends Controller
 {
     public function index(Item $item)
     {
+        session()->forget('selectedValue');
         $delivery_address = Auth::user()->profile()->select('postcode', 'address', 'building')->first();
 
         return view('purchase', compact('item', 'delivery_address'));
