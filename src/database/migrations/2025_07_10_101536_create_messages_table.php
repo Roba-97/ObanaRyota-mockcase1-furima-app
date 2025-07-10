895676f09f9b5ee690b3c1233633593ab58b20cf
Roba-97 <ryota@Roba.>
2025-07-10 13:32:30 +0900

    create: チャット用テーブルのマイグレーションファイル作成

---
+++ src/database/migrations/2025_07_10_101536_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chat_room_id')->constrained();
+            $table->foreignId('sender_id')->constrained('users');
+            $table->tinyInteger('content_type');  // 1:message, 2:img_path
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
